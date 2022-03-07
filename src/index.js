23da25289d0918e5fd93709245db11d3380b8f31
e-zhuk <82200530+e-zhuk@users.noreply.github.com>
2022-03-07 12:37:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    const numArr = [
+        [
+            'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+        ],
+        [
+            'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        ]
+    ];
+
+    const getDecimal = (num) => {
+        if (num < 20) {
+            return numArr[0][num];
+        }
+
+        const first = Math.floor(num / 10);
+        const second = num % 10;
+
+        const ten = numArr[1][first - 2];
+        const res = second ? numArr[0][second] : '';
+
+        return [ten, res].join(` `).trim();
+    };
+
+    if (number < 100) {
+        return getDecimal(number);
+    } else {
+        const first = Math.floor(number / 100);
+        const second = number % 100;
+
+        let hundred = first ? `${numArr[0][first]} hundred` : ``;
+        let tens = second ? getDecimal(second) : ``;
+
+        return [hundred, tens].join(` `).trim();
+    }
 }
